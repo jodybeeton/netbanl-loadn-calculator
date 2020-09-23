8671a85dfc45828d5fb4ffaf1cc5f1cba2ab1353
jody <misscosmo43@gmail.com>
2020-09-23 15:05:32 +0200

    first commit

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ _number_of_months = 12  # how long it would take you to repay the loan.
 total_repayment = loan_amount + (loan_amount * interest_rate)
 monthly_installment = total_repayment / _number_of_months
 
-print('Total Repayment: ', total_repayment)
-print("Monthly installment: ", monthly_installment)
-print("Please go to the nearest nedbank branch with you bank statement, ID and proof  of residence.")
+print('Total Repayment:', total_repayment)
+print("Monthly Installment:", monthly_installment)
+print("Please go tto the nearest Nedbank branch with your bank statement, ID and proof of residence.")
+
+if interest_rate <7:
+    print("The interest rate is less than 7")
+if interest_rate >24.5:
+    print("The interest rate is greater than 24.5")
+
+if interest_rate >= 7 and interest_rate <= 24.5:
+    print("Total Repayment and Monthly Installment")
+
